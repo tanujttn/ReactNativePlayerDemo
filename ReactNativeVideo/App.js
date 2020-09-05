cddf5f7f5b052c83027f4cd3d173abccd3b76104
Tanuj Sharma <tanuj.sharma@tothenew.com>
2020-09-05 15:03:30 +0530

    Initial commit

---
+++ ReactNativeVideo/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Video from 'react-native-video';
+import { StyleSheet } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Video source={{uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'}}   // Can be a URL or a local file.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       onBuffer={this.onBuffer}                // Callback when remote video is buffering
+       onError={this.videoError}               // Callback when video cannot be loaded
+       style={styles.backgroundVideo} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  }
+});
+
+export default App;
